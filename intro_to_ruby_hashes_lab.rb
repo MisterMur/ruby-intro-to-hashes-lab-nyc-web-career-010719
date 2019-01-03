ce32cb8e9f3f1dddf12ec791a52a290ec0cca74b
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 00:29:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,6 @@ def monopoly_with_third_tier
 	      :two_pieces_owned => 50,
 	      :three_pieces_owned => 100,
 	      :four_pieces_owend => 200
-	    
 	    },
 	    :names => {
 	      :reading_railroad => { },
